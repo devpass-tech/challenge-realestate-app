be5e17186566391d588b08461a2bb4e5aebcc023
Bruno Vieira Souza <bvsouza@yahoo.com.br>
2022-04-12 10:58:43 -0300

    implementação model favoritos

---
+++ solutions/devsprint-andre-nogueira-1/RealEstateApp/Models/Favorites.swift
@@ -0,0 +1,46 @@
+//
+//  Favorites.swift
+//  RealEstateApp
+//
+//  Created by Bruno Vieira Souza on 12/04/22.
+//
+
+import Foundation
+
+// MARK: - WelcomeElement
+struct WelcomeElement: Codable {
+    let welcomeDescription: String
+    let usableAreas: Int
+    let listingType, createdAt, listingStatus, id: String
+    let parkingSpaces: Int
+    let updatedAt: String
+    let owner: Bool
+    let images: [String]
+    let address: Address
+    let bathrooms, bedrooms: Int
+    let pricingInfos: PricingInfos
+
+}
+
+// MARK: - Address
+struct Address: Codable {
+    let city, neighborhood: String
+    let geoLocation: GeoLocation
+}
+
+// MARK: - GeoLocation
+struct GeoLocation: Codable {
+    let precision: String
+    let location: Location
+}
+
+// MARK: - Location
+struct Location: Codable {
+    let lon, lat: Double
+}
+
+// MARK: - PricingInfos
+struct PricingInfos: Codable {
+    let yearlyIptu, price, businessType, monthlyCondoFee: String
+    let period, rentalTotalPrice: String?
+}
