b3ab4a4e143a2f908cd4b5cb0f9c3f831a871cdd
douglas <lupim2008@gmail.com>
2022-04-22 17:59:42 -0300

    remove inits

--- solutions/devsprint-andre-nogueira-1/RealEstateApp/Screens/PropertyDetails/PropertyImageView.swift
+++ solutions/devsprint-andre-nogueira-1/RealEstateApp/Screens/PropertyDetails/PropertyImageView.swift
@@ -15,15 +15,6 @@ class PropertyImageView: BaseView, ViewCodable {
         return imageView
     }()
     
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setUpViewCode()
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
     func setUpHierarchy() {
         addSubview(propertyImage)
     }
