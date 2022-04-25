4d7b9e55b5487cc3e28412d5ba6cd5cb4b8b5b97
douglas <lupim2008@gmail.com>
2022-04-25 17:38:21 -0300

    test: function formattedPrice

--- solutions/devsprint-andre-nogueira-1/RealEstateAppTests/RealEstateAppTests.swift
+++ solutions/devsprint-andre-nogueira-1/RealEstateAppTests/RealEstateAppTests.swift
@@ -6,9 +6,15 @@
 //
 
 import XCTest
+@testable import RealEstateApp
 
 class RealEstateAppTests: XCTestCase {
-
+    
+    func testFormattedPrice() {
+        let result = String.formattedPrice(with: 100, and: 1000)
+        XCTAssertEqual(result, "Condomínio R$ 100   IPTU R$ 1000")
+    }
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
